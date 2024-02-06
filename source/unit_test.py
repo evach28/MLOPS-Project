da13f1ad34624793cac4d367480b1cea18a2503c
Joseph2505 <joseph.xu@efrei.net>
2024-02-06 11:43:40 +0100

    unit test

--- source/unit_test.py
+++ source/unit_test.py
@@ -3,7 +3,7 @@ import pandas as pd
 from fetch_data import fetch_data
 
 def test_fetch_data():
-    filepath = os.path.join(os.getcwd(), 'data', 'transfusion.data')
+    filepath = os.path.join(os.getcwd(), 'dataset', 'transfusion.data')
     data = fetch_data(filepath)
     assert not data.empty
     assert isinstance(data, pd.DataFrame)
